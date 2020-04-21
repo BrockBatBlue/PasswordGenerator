b7cfdaba1f87ee902bcd128829ad6fa0b10476b1
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-04-21 14:55:08 -0500

    added arrays for criteria

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ function generatePassword(){
   var upperCase = prompt("Would you like uppercase letters, y or n?");
   var numbers = prompt("Would you like numerical values, y or n?");
   var specialCharacters = prompt("Would you like special characters, y or n?");
-  console.log(length, upperCase, numbers, specialCharacters);
+  var letterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var numberArray = [0,1,2,3,4,5,6,7,8,9]
+  var specialArray = ["!",'"',"#","$","%","&","'","(",")","*","+",",",".","/",":",";","<",">","?","[","]","^","_","`","{","}","~","|"]
+  for(var i = 0; i< length; i++){
+
+  }
+  return generatedPassword 
 }
 
 // Add event listener to generate button
